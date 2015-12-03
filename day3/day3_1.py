05f06acab4f669cd90e8c9af4a9e2a0d27120129
brecht.vandevoort <brecht_vandevoort@hotmail.com>
2015-12-03 14:10:59 +0100

    Added day 3

---
+++ day3/day3_1.py
@@ -0,0 +1,4 @@
+p = [(0,0)]
+for x in [(-1,0) if x == '<' else (1,0) if x == '>' else (0,-1) if x == 'v' else (0,1) for x in open('input.txt').readline()]:
+    p.append(tuple(a+b for a,b in zip(x,p[-1])))
+print len(set(p))
