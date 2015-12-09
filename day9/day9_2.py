57f225a549b73e7ef78fc79dee85c43afc7693fb
brecht_vandevoort <brecht_vandevoort@hotmail.com>
2015-12-09 10:33:29 +0100

    Added day 9

---
+++ day9/day9_2.py
@@ -0,0 +1,5 @@
+import re,itertools
+l = [re.match('(\w*) to (\w*) = ([0-9]*)', x).groups() for x in open('input.txt')]
+c = list(set([x[0] for x in l]+[x[1] for x in l]))
+d = {tuple(sorted((x,y))):int(z) for x,y,z in l}
+print max([sum([d[tuple(sorted((p[i],p[i+1])))] for i in xrange(len(p)-1)]) for p in itertools.permutations(c)])
