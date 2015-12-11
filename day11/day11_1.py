fa7e7d2e7f5fc87487a54b0236d4bd712501fd2d
brecht_vandevoort <brecht_vandevoort@hotmail.com>
2015-12-11 09:36:38 +0100

    Added day 11

---
+++ day11/day11_1.py
@@ -0,0 +1,11 @@
+s = map(ord, list('hepxcrrq'))
+def i(p):
+    global s
+    s[p] += 1 if s[p] < 122 else -25
+    if s[p] == 97:
+        i(p-1)
+l = []
+while 105 in s or 108 in s or 111 in s or len([1 for x in xrange(6) if s[x] == s[x+1]-1 and s[x] == s[x+2]-2]) == 0 or len(l)<3 and (len(l)<2 or l[0] == l[1]-1):
+    i(7)
+    l = [x for x in xrange(7) if s[x] == s[x+1]]
+print ''.join(map(chr,s))
