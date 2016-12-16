9d7c9f21755310b4e4a2f7b05bc6ed3f8b8b664f
brecht.vandevoort <brecht.vandevoort@student.uhasselt.be>
2016-12-16 10:32:15 +0100

    Added day 16

---
+++ day_16/day16_2.py
@@ -0,0 +1,11 @@
+puzzle_input = '01110110101001000'
+disk_size = 35651584
+
+data = puzzle_input
+while len(data) < disk_size:
+	data += '0' + ''.join('0' if x == '1' else '1' for x in reversed(data))
+
+data = data[:disk_size]
+while len(data) % 2 == 0:
+	data = ''.join('1' if data[i] == data[i+1] else '0' for i in xrange(0,len(data), 2))
+print data
