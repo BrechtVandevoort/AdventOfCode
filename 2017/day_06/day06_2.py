c7e72092f0526a31be72f44bbaef78d8e6f273ed
Brecht Vandevoort <brechtvandevoort@gmail.com>
2017-12-06 08:55:27 +0100

    added day 6

---
+++ 2017/day_06/day06_2.py
@@ -0,0 +1,25 @@
+def redistribute(banks):
+	index = banks.index(max(banks))
+	value = banks[index]
+	banks[index] = 0
+	while value > 0:
+		index = (index + 1) % len(banks)
+		banks[index] += 1
+		value -= 1
+
+banks = map(int, open('input.txt').readline().split())
+seen = set()
+iterations = 0
+
+while tuple(banks) not in seen:
+	seen.add(tuple(banks))
+	iterations += 1
+	redistribute(banks)
+
+target = tuple(banks)
+loop_iterations = 0
+while tuple(banks) != target or loop_iterations == 0:
+	loop_iterations += 1
+	redistribute(banks)
+
+print loop_iterations
