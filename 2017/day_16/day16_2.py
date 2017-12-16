8e53f729afcea8df78a39ed1f3e8222227cdb75b
Brecht Vandevoort <brechtvandevoort@gmail.com>
2017-12-16 20:16:13 +0100

    added day 16

---
+++ 2017/day_16/day16_2.py
@@ -0,0 +1,25 @@
+instructions = open('input.txt').read().strip().split(',')
+order = list('abcdefghijklmnop')
+visited = []
+
+result = ''
+while len(visited) < 2 or result != visited[0]:
+	for instr in instructions:
+		if instr[0] == 's':
+			num = int(instr[1:])
+			order = order[-num:] + order[:-num]
+		else:
+			num1, num2 = 0, 0
+			if instr[0] == 'x':
+				num1, num2 = map(int, instr[1:].split('/'))
+			elif instr[0] == 'p':
+				c1, c2 = instr[1:].split('/')
+				num1 = order.index(c1)
+				num2 = order.index(c2)
+			order[num1], order[num2] = order[num2], order[num1]
+	result = ''.join(order)
+	visited.append(result)
+
+visited = visited[:-1]
+last_steps = 1000000000 % len(visited)
+print visited[last_steps-1]
